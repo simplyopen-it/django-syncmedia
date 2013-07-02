c993248a0fc9863a3cd4f29c11918708b83f3213
Marco Pattaro <mpattaro@gmail.com>
2013-07-02 18:05:25 +0200

    testing init

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from syncmedia import admin
+from syncmedia import management
+from syncmedia import managers
+from syncmedia import migrations
+from syncmedia import models
+from syncmedia import urls
+from syncmedia import views
