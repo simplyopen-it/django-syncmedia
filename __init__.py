fbd4e3c6e7b2b124e1cd30de567ec8c5cad8e6a7
Marco Pattaro <mpattaro@gmail.com>
2013-07-11 10:50:41 +0200

    new setup.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from syncmedia import admin
-from syncmedia import management
-from syncmedia import managers
-from syncmedia import migrations
-from syncmedia import models
-from syncmedia import urls
-from syncmedia import views
