2420136fdd14afc018962fa307ab226608065c12
Marco Pattaro <mpattaro@gmail.com>
2013-07-11 10:59:33 +0200

    fix in setup.py (missing coma)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     description='Syncmedia: data file sincronization for Django',
     long_description="Syncmedia is a django application that let you syncronize data files between multiple machines running the same django site.",
     author='Marco Pattaro, Matteo Atti, Michele Totaro, Dario Pavone',
-    author_email='mpattaro@gmail.it'
+    author_email='mpattaro@gmail.it',
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Framework :: Django",
@@ -31,36 +31,3 @@ setup(
         'syncmedia.commands',
     ],
 )
-# from setuptools import setup, find_packages
-
-# setup(
-#     name='django-syncmedia',
-#     version='0.1.3',
-#     description="Allow sync of static files for load balanced systems.",
-#     long_description=('TODO!!!'),
-#     keywords='sync, static',
-#     author='Matteo Atti, Marco Pattaro',
-#     author_email='matteo@***REMOVED***.com',
-#     maintainer='Matteo Atti',
-#     maintainer_email='matteo@***REMOVED***.com',
-#     license='MIT',
-#     package_dir={'syncmedia': 'syncmedia'},
-#     include_package_data=True,
-#     packages=find_packages(),
-#     classifiers=[
-#         'Development Status :: 5 - Production/Stable',
-#         'Environment :: Web Environment',
-#         'Framework :: Django',
-#         'Intended Audience :: Developers',
-#         'Intended Audience :: System Administrators',
-#         'License :: OSI Approved :: MIT License',
-#         'Operating System :: OS Independent',
-#         'Programming Language :: Python',
-#         'Programming Language :: Python :: 2.7',
-#         'Topic :: Internet :: Log Analysis',
-#         'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
-#         'Topic :: Security',
-#         'Topic :: System :: Logging',
-#     ],
-#     zip_safe=False,
-# )
