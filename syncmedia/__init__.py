bef40f85320746db8e2217059e9086a09b48a9a3
Marco Pattaro <mpattaro@gmail.com>
2013-09-05 12:01:39 +0200

    Add GUNICORN_WSGI reload command

--- syncmedia/__init__.py
+++ syncmedia/__init__.py
@@ -1,3 +1,13 @@
 # -*- coding: utf-8 -*-
 
 __version__ = "0.1.5"
+
+#################################################
+# Commands to reload different kind of services #
+#################################################
+from django.conf import settings
+
+GUNICONR_WSGI="""kill -HUP $(cat /var/run/gunicorn/%s.wsgi.pid)""" % \
+    settings.PROJECT_NAME
+
+# TODO: define other webserver's reload commands
