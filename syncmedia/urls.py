c2d1ef35b2aef560a1cc21b7de9889565183b407
Matteo Atti <attuch@gmail.com>
2013-09-05 11:45:54 +0200

    Added new url and func that call push or pull ...

--- syncmedia/urls.py
+++ syncmedia/urls.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import *
-from syncmedia.views import SyncKeys
+from syncmedia.views import SyncKeys, SyncMediaView
 
 urlpatterns = patterns(
     '',
-    url(r'^$', SyncKeys.as_view(), name="sync"),
+    url(r'^sync-command', SyncMediaView.as_view(), name="sync_media"),
+    url(r'^', SyncKeys.as_view(), name="sync"),
 )
