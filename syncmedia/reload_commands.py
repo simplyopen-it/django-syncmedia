f946fe38ebd52f7a486dabbead8bc7232fda3010
Marco Pattaro <mpattaro@gmail.com>
2013-09-06 17:07:23 +0200

    GUNICORN_WSGI restart command made more simple

--- syncmedia/reload_commands.py
+++ syncmedia/reload_commands.py
@@ -2,4 +2,6 @@
 
 from django.conf import settings
 
-GUNICORN_WSGI="""ps --ppid $(cat /var/run/gunicorn/%s.wsgi.pid) | grep python | tr -d " " | cut -d "?" -f1 | xargs -I{} kill -HUP {}""" % settings.PROJECT_NAME
+# GUNICORN_WSGI="""ps --ppid $(cat /var/run/gunicorn/%s.wsgi.pid) | grep python | tr -d " " | cut -d "?" -f1 | xargs -I{} kill -HUP {}""" % settings.PROJECT_NAME
+
+GUNICORN_WSGI="""ps -o pid --ppid $(cat /var/run/gunicorn/%s.wsgi.pid) --noheaders | xargs -I{} kill -HUP {}""" % settings.PROJECT_NAME
