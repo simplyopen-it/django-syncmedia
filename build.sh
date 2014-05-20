9ab88123e67acebcd1cea2d8daad6278427a98ab
Marco Pattaro <mpattaro@gmail.com>
2014-05-20 18:18:22 +0200

    auto push new tags

--- build.sh
+++ build.sh
@@ -1,43 +1,54 @@
 #!/bin/bash
 
-if [ x"$1" = x"" ]; then
-	echo "usage: `basename $0` <git branch|tag|commit>"
-	exit 1
+# Building dir (output package is left here too).
+BUILD="../BUILD"
+# Wether it must be a release build. If it's NOT a release build,
+# branch check is ignored and snapshot name is addedd to changelog
+# (Default: not release build).
+RELEASE="--ignore-branch -S"
+GITRELEASE="--git-ignore-branch"
+# Make a tag after successfull build, auto activete in release build
+# (Default: no).
+RETAG=""
+# Weter changelog must bu updated.
+CHANGELOG=1
+# tag/branch to build
+BRANCH=""
+# Version format used intag names
+VERSION_FORMAT='v%(version)s'
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+	"-b")
+	    shift
+	    BUILD="$1"
+	    ;;
+	"-R"|"--release")
+	    RELEASE="-R"
+	    RETAG="--git-tag --git-retag"
+	    GITRELEASE=""
+	    ;;
+	*) # Commit / tag / branch
+	    CHANGELOG=0
+	    BRANCH="--git-export=$1"
+	    ;;
+    esac
+    shift
+done
+
+if [[ ! -d "$BUILD" ]]; then
+    echo "$BUILD does not exist"
+    exit 1
 fi
 
-log_and_eval ()
-{
-	if [[ x"$@" != x"" ]]; then
-		echo "$@" 1>&2
-		eval "$@"
-	fi
-}
-
-APP_NAME="syncmedia"
-GIT_VERSION="$1"
-echo "git revision: $GIT_VERSION"
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "working dir: $DIR"
-cd "$DIR"
-NAME=$(grep "Source:" debian/control | awk -F": " '{ print $2 }')
-echo "project name: $NAME"
-VERSION=$(grep -m1 "__version__" "$APP_NAME"/__init__.py | awk -F"=" '{ print $2 }' | tr -d " " | tr -d "'" | tr -d "\"" | tr -d ",")
-echo "original version: $VERSION"
-TAR_NAME="$NAME""_$VERSION.orig.tar.gz"
-echo "archive name: $TAR_NAME"
-
-
-if [ ! -d ../BUILD ]; then
-	log_and_eval mkdir ../BUILD
+if [[ $CHANGELOG -gt 0 ]]; then
+    git-dch --git-author --debian-tag=$VESTION_FORMAT -a --spawn-editor=always $RELEASE --commit && \
+	git-buildpackage -us -uc --git-ignore-new --git-export-dir=$BUILD --git-debian-tag=$VERSION_FORMAT $GITRELEASE $RETAG
+    if [[ x$RETAG != "x" ]]; then
+	git push && git push --tags
+    fi
 else
-	log_and_eval rm -fr ../BUILD/*
+    git-buildpackage -us -uc --git-ignore-new --git-export-dir=$BUILD $BRANCH --git-debian-tag=$VERSION_FORMAT
 fi
 
-log_and_eval "git archive --format=tar \"$GIT_VERSION\" | gzip > ../\"$TAR_NAME\""
-
-log_and_eval cd ../BUILD
-log_and_eval tar zxf ../"$TAR_NAME"
-log_and_eval debuild -us -uc -tc
-EXIT=$?
-
-exit $EXIT
+exit 0
