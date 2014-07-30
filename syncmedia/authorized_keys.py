240a785698f5d896dd2871ee64f481e8b9c84345
Marco Pattaro <mpattaro@gmail.com>
2014-07-30 15:43:47 +0200

    fix missing key recongnition because of newline chars

--- syncmedia/authorized_keys.py
+++ syncmedia/authorized_keys.py
@@ -7,7 +7,7 @@ def get_keys(file_path):
             ret = fd.readlines()
     except IOError:
         ret = []
-    return [line.rstrip(os.linesep) for line in ret]
+    return ret
 
 def is_in(key, file_path):
     keys = get_keys(file_path)
@@ -16,24 +16,31 @@ def is_in(key, file_path):
 def add_key(key, file_path):
     if not is_in(key, file_path):
         with open(file_path, 'a') as fd:
-            fd.write(key + os.linesep)
+            if not key.endswith(os.linesep):
+                fd.write(key + os.linesep)
+            else:
+                fd.write(key)
 
 def add_keys(keys, file_path):
     authorized_keys = set(get_keys(file_path))
     keys = set(keys)
     not_auth = keys - authorized_keys
     with open(file_path, 'a') as fd:
-        fd.writelines([key + os.linesep for key in not_auth])
+        for key in not_auth:
+            if not key.endswith(os.linesep):
+                fd.write(key + os.linesep)
+            else:
+                fd.write(key)
 
 def del_key(key, file_path):
     keys = get_keys(file_path)
     keys.remove(key)
     with open(file_path, 'w') as fd:
-        fd.writelines([key + os.linesep for key in keys])
+        fd.writelines(keys)
 
 def del_keys(keys, file_path):
     authorized_keys = set(get_keys(file_path))
     keys = set(keys)
     to_write = authorized_keys - keys
     with open(file_path, 'w') as fd:
-        fd.writelines([key + os.linesep for key in to_write])
+        fd.writelines(to_write)
