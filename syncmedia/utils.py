bc04026fde46815cf012471f5cb447b166c13dec
Marco Pattaro <mpattaro@gmail.com>
2014-10-10 16:54:41 +0200

    get_auth_keys(), get_id_rsa()

--- syncmedia/utils.py
+++ syncmedia/utils.py
@@ -1,8 +1,14 @@
 import os
-
+from django.conf import settings
 
 def abspath(path):
     ret = os.path.abspath(path)
     if path.endswith(os.path.sep):
         ret += os.path.sep
     return ret
+
+def get_auth_keys():
+    return abspath(os.path.join(os.path.dirname(settings.PATH_RSA), 'authorized_keys'))
+
+def get_id_rsa():
+    return settings.PATH_RSA
